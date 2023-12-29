a8cbd82cc4a316759be5773dbfa2a1c0ce856ff0
Max Paulus 🥪 <maxpaulu@amazon.com>
2023-12-29 00:52:25 -0800

    todos plus check todos

--- main.go
+++ main.go
@@ -2,21 +2,57 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v5"
+	"github.com/maxpaulus43/go-pocketbase-htmx-templ-tailwind/models"
 	"github.com/maxpaulus43/go-pocketbase-htmx-templ-tailwind/views"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
+
+const (
+	TODOS = "todos"
+)
+
+func render(cmp templ.Component, c echo.Context) error {
+	return cmp.Render(c.Request().Context(), c.Response().Writer)
+}
 
 func main() {
 	app := pocketbase.New()
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/", func(c echo.Context) error {
-			return views.Index().Render(c.Request().Context(), c.Response().Writer)
+			return render(views.Index(), c)
+		})
+
+		e.Router.GET("/todos", func(c echo.Context) error {
+			query := app.Dao().RecordQuery(TODOS).
+				AndWhere(dbx.HashExp{"is_complete": false}).
+				Limit(10)
+			todos := []models.Todo{}
+			if err := query.All(&todos); err != nil {
+				return err
+			}
+			return render(views.Todos(todos), c)
+		})
+
+		e.Router.POST("/check/:id", func(c echo.Context) error {
+			record, err := app.Dao().FindRecordById(TODOS, c.PathParam("id"))
+			if err != nil {
+				return err
+			}
+			var isComplete bool = record.Get("is_complete").(bool)
+			record.Set("is_complete", !isComplete)
+			if err := app.Dao().SaveRecord(record); err != nil {
+				return err
+			}
+			return c.Redirect(http.StatusFound, "/todos")
 		})
 
 		// serves static files from the provided public dir
---
+++ models/Todo.go
@@ -0,0 +1,7 @@
+package models
+
+type Todo struct {
+	Id         string `db:"id"`
+	Content    string `db:"content"`
+	IsComplete bool   `db:"is_complete"`
+}
